1575e4f4dd0e8d8669f404b0e5c3833e6fc8ece3
Jenseko <jenscode23@gmail.com>
2023-06-05 11:19:51 +0200

    setup

---
+++ Model/Contact.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from 'mongoose';
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: String,
+},
+);
+
+export const Contact = mongoose.model("Contact", contactSchema);
+
---
+++ seed.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { Contact } from "./Model/Contact.js";
+
+
+await mongoose.connect("mongodb://localhost:27017/contact-with-mongodb")
+    .then(console.log)
+    .catch(console.error)
+
+
+// Speichern/Create eines neues Users
+const user = new Contact();
+user.name = "Billy Smith";
+user.email = "billy@shmithysmith.com";
+await user.save();
+
+
+// Option 2: User zu erstellen
+const user2 = new Contact({ name: "Sarah Parker", email: "parker@park.park" });
+await user2.save();
+
+
+// Vorteil: Speichert direkt den User ohne save() schreiben zu müssen
+await Contact.create({ name: "Jessica Smith", email: "jessice@smithysmith.com" });
+
+mongoose.disconnect()
